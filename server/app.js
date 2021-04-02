d8d3ab4583f1e1d3b83b478a9fa15b26b4103759
William Villeneuve <ciiqr@users.noreply.github.com>
2021-04-02 14:43:26 -0400

    wip

---
+++ server/app.js
@@ -0,0 +1,10 @@
+// TODO: install koa & others
+// TODO: maybe rename this project folder: server => backend
+const Koa = require('koa');
+
+const app = new Koa();
+
+// TODO: setup app
+// app.use(/* register your middleware as normal */);
+
+module.exports = app;
