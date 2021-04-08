959b05c7b2174221f852ca4b7f3e80436b84eac7
William Villeneuve <ciiqr@users.noreply.github.com>
2021-04-08 19:31:12 -0400

    build time support for hosting on a subpath

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 import Requester from './pages/Requester';
 import Sender from './pages/Sender';
 
+// to support hosting on a subpath
+const baseUrl = new URL(document.baseURI);
+const basePath = baseUrl.pathname;
+
 function App() {
     return (
         <Switch>
-            <Route path="/" component={Requester} exact />
-            <Route path="/:id" component={Sender} exact />
+            <Route path={`${basePath}`} component={Requester} exact />
+            <Route path={`${basePath}:id`} component={Sender} exact />
             <Route>Much wow</Route>
         </Switch>
     );
