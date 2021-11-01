751ad16704ea8c967045073b53ca7f671adf13a9
William Villeneuve <ciiqr@users.noreply.github.com>
2021-10-31 23:54:20 -0400

    switch to the-new-css-reset

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'marx-css/css/marx.css';
+import 'the-new-css-reset/css/reset.css';
 import 'index.css';
 import App from 'App';
 import * as serviceWorker from 'serviceWorker';
